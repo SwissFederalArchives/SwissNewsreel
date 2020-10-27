3f753a3ade39ac70d757e467e2bd1653cd1e2317
André Ourednik <70896290+andreobar@users.noreply.github.com>
2020-10-28 00:20:59 +0100

    Create SwissNewsReel.R

---
+++ SwissNewsReel.R
@@ -0,0 +1,136 @@
+library(httr)
+library(jsonlite)
+library(data.table)
+library(stringr)
+library(ggplot2)
+library(magrittr)
+library(svglite)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Functions ----
+makerequest <- function(skip,take) {
+  print(paste("requesting records",skip,":",skip+take,"from",totalpages))
+  jsonrequest <- paste0('{
+    "query":{
+      "searchGroups":[
+        {"searchFields":[
+          {"value":"J2.143#1996/386*","key":"referenceCode"}
+        ],"fieldOperator":1}
+      ],
+      "groupOperator":1
+    },
+    "paging":{"skip":', skip ,',"take":', take ,',"orderBy":"","sortOrder":""},
+    "facetsFilters": [
+      {
+        "filters": ["level:\\"Dokument\\""],
+        "facet": "level"
+      },
+      {
+        "filters": [
+          "aggregationFields.bestand:\\"Stiftung Schweizer Filmwochenschau (1942-1975)\\""
+        ],
+        "facet": "aggregationFields.bestand"
+      }
+    ]
+  }')
+  POST(
+    url,
+    body = minify(jsonrequest), 
+    encode = "raw",
+    content_type_json()
+  )
+}
+
+getDescription <- function(id) {
+  print(paste("fetching record",id,"(",counter,"/",total,")"))
+  counter <<- counter + 1
+  resget <- GET(
+    paste0("https://www.recherche.bar.admin.ch/recherche/api/v1/entities/",id), 
+    encode = "json",
+    content_type_json()
+  )
+  content(resget)
+}
+
+# Send a first request to find the total number of pages  ----
+url <- "https://www.recherche.bar.admin.ch/recherche/api/v1/entities/Search"
+res <- makerequest(0,1)
+status_code(res)
+headers(res)
+fws <- content(res)
+totalpages <- fws$entities$paging$total
+
+# Fetch all data by bunches of 100 ----
+stepstarts <- seq(1, ceiling(totalpages/100)*100, by = 100)
+fws.datatable <- lapply(stepstarts,function(i){
+  res <- makerequest(i,100)
+  fws <- content(res)
+  data.table(
+    refCode = sapply(fws$entities$items,function(x) return(x$archiveRecordId)),
+    archiveID = sapply(fws$entities$items,function(x) return(x$referenceCode)),
+    level = sapply(fws$entities$items,function(x) return(x$level)),
+    date = sapply(fws$entities$items,function(x) return(x$creationPeriod$text)),
+    title = sapply(fws$entities$items,function(x) return(x$title)),
+    dauer = sapply(fws$entities$items,function(x) return(x$customFields$format %>% unlist)),
+    url = sapply(fws$entities$items,function(x) return(x$customFields$digitaleVersion %>% unlist %>% .["url"])),
+    thema = sapply(fws$entities$items, function(x) x$customFields$thema %>% unlist)
+  )
+}) %>% rbindlist(fill=TRUE)
+
+# Fetch extra data from individual records ----
+counter <- 1
+total <- nrow(fws.datatable)
+fws.datatable[, darin := lapply(refCode, function(x) {x = getDescription(x); x[["withinInfo"]] %>% unlist})]
+
+save(fws.datatable,file="FilmWochenSchau.RData")
+
+# Extract variables with RegEx ----
+# load("FilmWochenSchau.RData") 
+
+# First, clean the data. Some columns could have been turned to lists due to API reply inconsistency
+unlistColumn <- function(column) {
+  sapply(column, function(x) {
+    if (!is.null(unlist(x))) {return(unlist(x))} else return(NA)
+  })
+}
+fws.datatable$refCode <- unlistColumn(fws.datatable$refCode)
+fws.datatable$archiveID <- unlistColumn(fws.datatable$archiveID)
+fws.datatable$level <- unlistColumn(fws.datatable$level)
+fws.datatable$date <- unlistColumn(fws.datatable$date)
+fws.datatable$title <- unlistColumn(fws.datatable$title)
+fws.datatable$dauer <- unlistColumn(fws.datatable$dauer)
+fws.datatable$url <- unlistColumn(fws.datatable$url)
+fws.datatable$thema <- unlistColumn(fws.datatable$thema)
+
+# Then apply RegEx
+fws.datatable[,description_genre := sapply(darin, function(x) {str_match(x, "Genre:[\\r\\n ]{1,2}([^\\r\\n]*)") %>% .[2]})]
+fws.datatable[,description_inhaltsangabe := sapply(darin, function(x) {str_match(x, "Inhaltsangabe:[\\r\\n ]{1,2}([^\\r\\n]*)") %>% .[2]})]
+fws.datatable[,description_inhaltsangabe_ort := sapply(description_inhaltsangabe, function(x) {str_match(x, "^([^:]*)") %>% .[2]})]
+fws.datatable[,thema_orte := sapply(thema, function(x) {str_match(x, "Orte:[\\r\\n ]{1,3}([^\\r\\n]*)") %>% .[2]})]
+fws.datatable[,thema_schlagworte := sapply(thema, function(x) {str_match(x, "Schlagworte:[\\r\\n ]{1,3}([^\\r\\n]*)") %>% .[2]})]
+fws.datatable[,dauer_dauer := sapply(dauer, function(x) {str_match(x, "Dauer: ([0-9:]*)") %>% .[2]})]
+fws.datatable[,dauer_seconds := sapply(dauer_dauer, function(x) {
+  ifelse(
+    str_count(x,":")>1,
+    as.difftime(x, format = "%H:%M:%S", units = "secs") %>% strtoi,
+    as.difftime(x, format = "%M:%S", units = "secs") %>% strtoi
+  )
+})]
+
+
+# Visualisation ----
+fws.datatable[,date:=as.Date(date,"%d.%m.%Y")]
+emissions_par_month <- emissions_par_date[, .N, by=.(month,year)]
+ggplot(emissions_par_month) +
+  geom_col(aes(x=month,y=N),colour="white", fill="darkred") + 
+  scale_x_continuous(
+    breaks=c(3,6,9,12),
+    minor_breaks = c(1,2,4,5,7,8,10,11)
+  ) +
+  coord_polar(start=pi/12) +
+  facet_wrap(~year,ncol=10) +
+  labs(title = "Ciné-Journal suisse",
+       subtitle = "Nombre d'émissions diffusées par année et mois",
+       caption = "Source: Archives fédérales suisses", 
+       x = NULL, y = NULL) 
+ggsave("cinejournal.svg",width=15,height=8)
